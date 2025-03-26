a8401fcb52aa61fb81390ce8328c5f4e78c1f93f
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-26 17:19:10 -0300

    feat(repositories): add UsersRepository Create method

--- backend/src/repositories/users.go
+++ backend/src/repositories/users.go
@@ -1,6 +1,9 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"github.com/joaooliveira247/backend-test/src/models"
+	"gorm.io/gorm"
+)
 
 type UsersRepository struct {
 	db *gorm.DB
@@ -8,4 +11,14 @@ type UsersRepository struct {
 
 func NewUsersRepository(db *gorm.DB) UsersRepository {
 	return UsersRepository{db}
+}
+
+func (repository *UsersRepository) Create(user *models.Users) (string, error) {
+	result := repository.db.Create(&user)
+
+	if err := result.Error; err != nil {
+		return "", err
+	}
+
+	return user.AffiliateCode, nil
 }
