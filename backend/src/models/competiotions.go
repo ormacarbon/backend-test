8fc5f8ab60c4f27b2b97a7bd1c76ec67de208fbf
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-27 16:57:50 -0300

    feat(models): add Competitions IsEmpty method

--- backend/src/models/competiotions.go
+++ backend/src/models/competiotions.go
@@ -1,6 +1,12 @@
 package models
 
+import "github.com/google/uuid"
+
 type Competitions struct {
 	BaseModel
 	Status bool `json:"status" gorm:"type:boolean;not null;column:status"`
+}
+
+func (comp *Competitions) IsEmpty() bool {
+	return comp.ID == uuid.Nil
 }
