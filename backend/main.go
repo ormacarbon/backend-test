fc19e4c0b103730b6c2947b3a36f5900a813f644
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-27 19:50:25 -0300

    feat(main) add main func to run project

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"log"
+
+	"github.com/gin-gonic/gin"
 	"github.com/joaooliveira247/backend-test/src/config"
+	"github.com/joaooliveira247/backend-test/src/db"
+	"github.com/joaooliveira247/backend-test/src/routes"
 )
 
 func init() {
 	config.LoadEnv()
+}
+
+func main() {
+
+	gormDB, err := db.GetDBConnection()
+
+	if err != nil {
+		log.Fatalf("DATABASE Connection: %v", err)
+	}
+
+	if err := db.CreateTables(gormDB); err != nil {
+		log.Fatalf("DATABASE Tables Creation: %v", err)
+		return
+	}
+
+	api := gin.Default()
+	routes.RegistryRoutes(api)
+
+	if err := api.Run(":8000"); err != nil {
+		log.Fatalf("API RUN: err")
+	}
 }
