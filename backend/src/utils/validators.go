4c5c806b717a62de5fb2d37b7bab49b48524a7a4
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-27 19:33:06 -0300

    feat(utils): add EmailValidator func

---
+++ backend/src/utils/validators.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"net"
+	"regexp"
+	"strings"
+)
+
+func EmailValidator(email string) bool {
+	regex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(regex)
+	if !re.MatchString(email) {
+		return false
+	}
+
+	parts := strings.Split(email, "@")
+	if len(parts) != 2 {
+		return false
+	}
+	mxRecords, err := net.LookupMX(parts[1])
+	return err == nil && len(mxRecords) > 0
+}
