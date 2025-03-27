60c62a991983fe63a4513dc38bb0d8270f1a3de1
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-27 19:38:08 -0300

    feat(controllers): add CompetitionsController Create method

--- backend/src/controllers/competitions.go
+++ backend/src/controllers/competitions.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joaooliveira247/backend-test/src/models"
 	"github.com/joaooliveira247/backend-test/src/repositories"
 )
 
@@ -16,4 +20,32 @@ func NewCompetitionsController(
 	pointRepo repositories.PointsRepository,
 ) *CompetitionsController {
 	return &CompetitionsController{userRepo, compRepo, pointRepo}
+}
+
+func (ctrl *CompetitionsController) Create(ctx *gin.Context) {
+	compCheck, _ := ctrl.CompetitionRepository.GetActiveCompetition()
+
+	if compCheck.IsEmpty() {
+		id, err := ctrl.CompetitionRepository.Create(
+			&models.Competitions{Status: true},
+		)
+
+		if err != nil {
+			ctx.JSON(
+				http.StatusInternalServerError,
+				gin.H{"error": "create competition", "details": err.Error()},
+			)
+			return
+		}
+		ctx.JSON(http.StatusCreated, gin.H{"id": id})
+		return
+	}
+
+	ctx.JSON(
+		http.StatusConflict,
+		gin.H{
+			"error":   "competition already activated",
+			"details": compCheck.ID,
+		},
+	)
 }
