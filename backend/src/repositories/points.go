af23e3bdfac1c57c5bbd0ab904a5dec8e5ea3466
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-27 17:01:31 -0300

    feat(repositories): add PointsRepository AddPoint method

--- backend/src/repositories/points.go
+++ backend/src/repositories/points.go
@@ -1,6 +1,9 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"github.com/joaooliveira247/backend-test/src/models"
+	"gorm.io/gorm"
+)
 
 type PointsRepository struct {
 	db *gorm.DB
@@ -8,4 +11,12 @@ type PointsRepository struct {
 
 func NewPointsRepository(db *gorm.DB) PointsRepository {
 	return PointsRepository{db}
+}
+
+func (repository *PointsRepository) AddPoint(point *models.Points) error {
+	if err := repository.db.Create(point).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
