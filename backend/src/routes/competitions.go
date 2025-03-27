ebbab655f00114637085d59d6f9ddcce4ef108fb
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-27 19:44:34 -0300

    feat(routes): add CompetitionsRoute func

---
+++ backend/src/routes/competitions.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joaooliveira247/backend-test/src/controllers"
+	"github.com/joaooliveira247/backend-test/src/db"
+	"github.com/joaooliveira247/backend-test/src/repositories"
+)
+
+func CompetitionsRoute(eng *gin.Engine) {
+	gormDB, err := db.GetDBConnection()
+
+	if err != nil {
+		log.Fatalf("Users Route: %v", err)
+	}
+
+	usersReposiotry := repositories.NewUsersRepository(gormDB)
+	compsRepository := repositories.NewCompetiotionsRepository(gormDB)
+	pointsRepository := repositories.NewPointsRepository(gormDB)
+
+	controller := controllers.NewCompetitionsController(
+		usersReposiotry,
+		compsRepository,
+		pointsRepository,
+	)
+
+	competitionsGroup := eng.Group("/competitions")
+	{
+		competitionsGroup.POST("/", controller.Create)
+		competitionsGroup.GET("/", controller.GetCompetition)
+		competitionsGroup.PUT("/", controller.CloseCompetition)
+	}
+}
