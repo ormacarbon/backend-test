0598df052154febb983f410ada99ea27ae3a1e5a
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-27 19:42:36 -0300

    feat(controllers): add CompetitionsController CloseCompetition method

--- backend/src/controllers/competitions.go
+++ backend/src/controllers/competitions.go
@@ -1,11 +1,14 @@
 package controllers
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/joaooliveira247/backend-test/src/models"
 	"github.com/joaooliveira247/backend-test/src/repositories"
+	"gorm.io/gorm"
 )
 
 type CompetitionsController struct {
@@ -70,5 +73,38 @@ func (ctrl *CompetitionsController) GetCompetition(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, compCheck)
 		return
 	}
+	ctx.JSON(http.StatusNoContent, nil)
+}
+
+func (ctrl *CompetitionsController) CloseCompetition(ctx *gin.Context) {
+	compID, err := uuid.Parse(ctx.Query("ID"))
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid id format", "details": err.Error(),
+		})
+		return
+	}
+
+	if err := ctrl.CompetitionRepository.CloseCompetition(compID); err != nil {
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(
+				http.StatusNotFound,
+				gin.H{
+					"error":   "competition not found",
+					"details": err.Error(),
+				},
+			)
+			return
+		}
+
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "closed competition error", "details": err.Error()},
+		)
+		return
+	}
+
 	ctx.JSON(http.StatusNoContent, nil)
 }
