c077485b9c67692d41596996ad85a6ec23edcc65
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-27 17:00:32 -0300

    feat(repositories): add NewPointsRepository func

--- backend/src/repositories/points.go
+++ backend/src/repositories/points.go
@@ -2,7 +2,10 @@ package repositories
 
 import "gorm.io/gorm"
 
-
 type PointsRepository struct {
 	db *gorm.DB
+}
+
+func NewPointsRepository(db *gorm.DB) PointsRepository {
+	return PointsRepository{db}
 }
