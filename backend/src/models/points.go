00e519ed42092e054cb1a0bcb86057114bdf1f0f
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-28 10:24:04 -0300

    fix(models) add Points model CompetitionID and userID gorm field tags

--- backend/src/models/points.go
+++ backend/src/models/points.go
@@ -4,6 +4,6 @@ import "github.com/google/uuid"
 
 type Points struct {
 	BaseModel
-	CompetitionID uuid.UUID
-	UserID uuid.UUID
+	CompetitionID uuid.UUID `gorm:"type:uuid;column:competition_id"`
+	UserID uuid.UUID `gorm:"type:uuid;column:user_id"`
 }
