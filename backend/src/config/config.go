585e4ddf2711c7282bc8d0d60fa5942ab55afc9b
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-28 19:01:29 -0300

    feat(config): add SERVICE_EMAIL var

--- backend/src/config/config.go
+++ backend/src/config/config.go
@@ -3,9 +3,11 @@ package config
 import "os"
 
 var (
-	DATABASE_URL = ""
+	DATABASE_URL   = ""
+	SERVICE_EMAIL = ""
 )
 
 func LoadEnv() {
 	DATABASE_URL = os.Getenv("DATABASE_URL")
+	SERVICE_EMAIL = os.Getenv("SERVICE_EMAIL")
 }
