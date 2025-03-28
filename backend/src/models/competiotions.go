bd74a01772b3f1884645331f5a78a8da30f9298f
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-28 10:24:50 -0300

    feat(models) add CompetitionReport model

--- backend/src/models/competiotions.go
+++ backend/src/models/competiotions.go
@@ -7,6 +7,13 @@ type Competitions struct {
 	Status bool `json:"status" gorm:"type:boolean;not null;column:status"`
 }
 
+type CompetitionReport struct {
+	Name string `json:"name" gorm:"column:name"`
+	Points uint64 `json:"points" gorm:"column:points"`
+	Email string `json:"-" gorm:"column:email"` 
+	Phone string `json:"-" gorm:"column:phone"`
+}
+
 func (comp *Competitions) IsEmpty() bool {
 	return comp.ID == uuid.Nil
 }
