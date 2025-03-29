be2e98fb8e62eab4352245993a331b6586141cd2
joaooliveira247 <joaooliveira247@gmail.com>
2025-03-29 08:44:55 -0300

    feat(middlewares): add getLimiter func

--- backend/src/middlewares/limit.go
+++ backend/src/middlewares/limit.go
@@ -2,6 +2,19 @@ package middlewares
 
 import (
 	"sync"
+
+	"golang.org/x/time/rate"
 )
 
 var limiters = sync.Map{}
+
+func getLimiter(ip string) *rate.Limiter {
+	limiter, ok := limiters.Load(ip)
+
+	if !ok {
+		newLimiter := rate.NewLimiter(1, 5)
+		limiters.Store(ip, newLimiter)
+		return newLimiter
+	}
+	return limiter.(*rate.Limiter)
+}
