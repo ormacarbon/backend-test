f416b5bf0b13f4ba954ac8e461647b1b72f65bca
João Otávio Peccia <cnttspk@gmail.com>
2025-04-02 10:52:20 -0300

    style: adding a gorm unique missing in email

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Name         string `json:"name"`
-	Email        string `json:"email"`
-	PhoneNumber  string `json"phone_number"`
+	Email        string `json:"email" gorm:"unique"`
+	PhoneNumber  string `json:"phone_number"`
 	ReferralCode string `json:"referral_code" gorm:"unique"`
 	ReferredBy   string `json:"referred_by"`
 	Points       int    `json:"points"`
