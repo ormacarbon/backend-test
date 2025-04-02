2a2ca42d42007351aad5ba5bf4b44193a3a59435
João Otávio Peccia <cnttspk@gmail.com>
2025-04-02 11:32:07 -0300

    feat: add setup routes in main.go

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/jpeccia/go-backend-test/config"
+	"github.com/jpeccia/go-backend-test/internal/handlers"
+	"github.com/jpeccia/go-backend-test/internal/repositories"
+	"github.com/jpeccia/go-backend-test/internal/routes"
+	"github.com/jpeccia/go-backend-test/internal/services"
 )
 
 func main() {
@@ -13,9 +17,15 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	config.ConnectDatabase()
-
+	
 	r := gin.Default()
+
+	userRepo := repositories.NewUserRepository(config.DB)
+	userService := services.NewUserService(userRepo)
+	userHandler := handlers.NewUserHandler(userService)
+
+	routes.SetupRoutes(r, userHandler)
+
 	r.Run(":8080")
 }
