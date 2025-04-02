f6b95272e283a6f121e9697438aee2f3f5f48023
João Otávio Peccia <cnttspk@gmail.com>
2025-04-02 11:01:41 -0300

    feat: create User Repository and Create User e FindUserByEmail functions

---
+++ internal/repositories/user_repository.go
@@ -0,0 +1,30 @@
+package repositories
+
+import (
+	"github.com/jpeccia/go-backend-test/internal/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	CreateUser(user *models.User) error
+	FindUserByEmail(email string) (*models.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db}
+}
+
+func (u *userRepository) CreateUser(user *models.User) error {
+	return u.db.Create(user).Error
+}
+
+func (u *userRepository) FindUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	err := u.db.Where("email= ?", email).First(&user).Error
+	return &user, err
+}
+
