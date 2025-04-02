1e39b8ebff5147fd22decb98324bda745a55f289
João Otávio Peccia <cnttspk@gmail.com>
2025-04-02 11:16:31 -0300

    feat: create User Handler

---
+++ internal/handlers/user_handler.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jpeccia/go-backend-test/internal/dto"
+	"github.com/jpeccia/go-backend-test/internal/services"
+)
+
+type UserHandler struct {
+	userService services.UserService
+}
+
+func NewUserHandler(userService services.UserService) *UserHandler {
+	return &UserHandler{userService}
+}
+
+func (u *UserHandler) RegisterUser(c *gin.Context) {
+	var input dto.RegisterUserDTO
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := u.userService.RegisterUser(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "User registered successfully",
+		"referral_code": user.ReferralCode,
+	})
+}
