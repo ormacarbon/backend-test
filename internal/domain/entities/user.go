d7e12842df79e909267cbd02ad1602cfbd76838d
cassius <cassiusbessa@gmail.com>
2025-04-03 20:32:56 -0300

    refactor: :recycle: add password to user

--- internal/domain/entities/user.go
+++ internal/domain/entities/user.go
@@ -6,16 +6,17 @@ import (
 )
 
 type User struct {
-	name  string
-	email object_values.Email
-	phone object_values.PhoneNumber
+	name     string
+	email    object_values.Email
+	phone    object_values.PhoneNumber
+	password object_values.Password
 }
 
-func NewUser(name string, email object_values.Email, phone object_values.PhoneNumber) (User, error) {
+func NewUser(name string, email object_values.Email, hashed_pass object_values.Password, phone object_values.PhoneNumber) (User, error) {
 	if name == "" {
 		return User{}, shared.ErrValidation
 	}
-	return User{name: name, email: email, phone: phone}, nil
+	return User{name: name, password: hashed_pass, email: email, phone: phone}, nil
 }
 
 func (u User) Name() string {
@@ -24,6 +25,10 @@ func (u User) Name() string {
 
 func (u User) Email() object_values.Email {
 	return u.email
+}
+
+func (u User) Password() object_values.Password {
+	return u.password
 }
 
 func (u User) Phone() object_values.PhoneNumber {
