df5616ae89f32a95ab7a24e493480f2c17f6c133
João Otávio Peccia <cnttspk@gmail.com>
2025-04-03 12:22:56 -0300

    fix: load env error in deploy

--- config/config.go
+++ config/config.go
@@ -2,13 +2,16 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("RENDER") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Aviso: Nenhum arquivo .env encontrado. Usando variáveis de ambiente do sistema.")
+		}
 	}
 }
