3f15a0a27845b02c2511e89f03d19f9eeda6d9dd
cassius <cassiusbessa@gmail.com>
2025-04-03 18:56:58 -0300

    refactor: :recycle: make email imutable

--- internal/domain/object-values/email.go
+++ internal/domain/object-values/email.go
@@ -9,15 +9,18 @@ type Email struct {
 	value string
 }
 
-var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+var (
+	emailRegex      = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	errInvalidEmail = errors.New("invalid email format")
+)
 
-func NewEmail(email string) (*Email, error) {
+func NewEmail(email string) (Email, error) {
 	if !emailRegex.MatchString(email) {
-		return nil, errors.New("email inválido")
+		return Email{}, errInvalidEmail
 	}
-	return &Email{value: email}, nil
+	return Email{value: email}, nil
 }
 
-func (e *Email) Value() string {
+func (e Email) Value() string {
 	return e.value
 }
