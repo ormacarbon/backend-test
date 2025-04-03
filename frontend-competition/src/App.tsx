3828237b0acf89a2c824f57dc9eb3d86b7939926
João Otávio Peccia <cnttspk@gmail.com>
2025-04-02 21:18:00 -0300

    feat: create API connection file

--- frontend-competition/src/App.tsx
+++ frontend-competition/src/App.tsx
@@ -1,9 +1,14 @@
-import './index.css'
+import AppRoutes from "./routes/routes";
 
-function App() {
-
+export default function App() {
   return (
-  )
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      <header className="p-4 bg-white shadow-md">
+        <h1 className="text-xl font-bold text-center">Competição de Carbono</h1>
+      </header>
+      <main className="flex-1 flex justify-center items-center">
+        <AppRoutes />
+      </main>
+    </div>
+  );
 }
-
-export default App
