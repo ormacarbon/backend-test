0478fe1674cc8fd9fa71c6c01d2e95462fde5066
cassius <cassiusbessa@gmail.com>
2025-04-04 19:45:12 -0300

    test: :white_check_mark: ensures that users are created with a starting point

--- internal/application/dto/user-dto.go
+++ internal/application/dto/user-dto.go
@@ -1,10 +1,11 @@
 package dto
 
 type CreateUserInput struct {
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-	Phone    string `json:"phone" binding:"required"`
+	Name      string `json:"name" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required"`
+	Phone     string `json:"phone" binding:"required"`
+	InvitedBy string `json:"invited_by"`
 }
 
 type CreateUserOutput struct {
--- internal/domain/entities/user_test.go
+++ internal/domain/entities/user_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/cassiusbessa/backend-test/internal/domain/entities"
 	object_values "github.com/cassiusbessa/backend-test/internal/domain/object-values"
+	"github.com/google/uuid"
 )
 
 func TestNewUser(t *testing.T) {
@@ -13,20 +14,25 @@ func TestNewUser(t *testing.T) {
 		name        string
 		email       string
 		phone       string
+		invitedBy   *uuid.UUID
 		expectError bool
 	}{
-		{"John Doe", "user@example.com", "+14155552671", false},
-		{"", "user@example.com", "+14155552671", true},
+		{"John Doe", "user@example.com", "+14155552671", nil, false},
+		{"", "user@example.com", "+14155552671", nil, true},
 	}
 
 	for _, tt := range tests {
 		email, _ := object_values.NewEmail(tt.email)
 		phone, _ := object_values.NewPhoneNumber(tt.phone)
-
 		password, _ := object_values.NewPassword("defaultPassword123")
-		_, err := entities.NewUser(tt.name, email, password, phone, nil)
+
+		u, err := entities.NewUser(tt.name, email, password, phone, tt.invitedBy)
 		if (err != nil) != tt.expectError {
 			t.Errorf("NewUser(%s, %s, %s) expected error: %v, got: %v", tt.name, tt.email, tt.phone, tt.expectError, err)
+		}
+
+		if err == nil && u.Points() != 1 {
+			t.Errorf("expected initial points to be 1, got %d", u.Points())
 		}
 	}
 }
