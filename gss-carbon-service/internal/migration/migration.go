881be29fc497d331dec1600571fb2e4dda11a166
Israel Schroeder Moreira <israelschroederm@gmail.com>
2025-04-03 23:51:47 -0300

    feat: created migrations

--- gss-carbon-service/internal/migration/migration.go
+++ gss-carbon-service/internal/migration/migration.go
@@ -3,21 +3,42 @@ package migration
 import (
 	"github.com/go-gormigrate/gormigrate/v2"
 	"github.com/icl00ud/backend-test/internal/model"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
-// Migrate executa as migrações usando gormigrate.
-func Migrate(db *gorm.DB) error {
+func Migrate(db *gorm.DB, logger *zap.Logger) error {
+	sugar := logger.Sugar()
+	sugar.Info("Starting database migrations...")
+
 	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
 		{
 			ID: "20230403_create_users_table",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.AutoMigrate(&model.User{})
+				sugar.Infow("Running migration", "id", "20230403_create_users_table")
+				err := tx.AutoMigrate(&model.User{})
+				if err != nil {
+					sugar.Errorw("Migration failed", "id", "20230403_create_users_table", "error", err)
+				}
+				return err
 			},
 			Rollback: func(tx *gorm.DB) error {
-				return tx.Migrator().DropTable("users")
+				sugar.Infow("Rolling back migration", "id", "20230403_create_users_table")
+				err := tx.Migrator().DropTable("users")
+				if err != nil {
+					sugar.Errorw("Migration rollback failed", "id", "20230403_create_users_table", "error", err)
+				}
+				return err
 			},
 		},
 	})
-	return m.Migrate()
+
+	err := m.Migrate()
+	if err != nil {
+		sugar.Errorw("Migration process failed", "error", err)
+		return err
+	}
+
+	sugar.Info("Database migrations finished successfully.")
+	return nil
 }
