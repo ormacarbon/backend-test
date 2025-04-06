ed854414f5ccb8e84d0084c1747604bf5ed26011
cassius <cassiusbessa@gmail.com>
2025-04-06 05:18:28 -0300

    refactor: :art: use env to base backend url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   images: { unoptimized: true },
 };
 
